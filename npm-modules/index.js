8135a5eb0d91b88979eb5d447256bbdfd502a94b
TadeunASCIImento <tadeunascimento36@gmail.com>
2020-09-18 10:56:35 -0300

    Importando e usando módulos de terceiros com npm

---
+++ npm-modules/index.js
@@ -0,0 +1,16 @@
+// Módulos
+    // módulo lodash importado para o projeto
+    const _ = require('lodash');
+    
+    // Array de números inteiros usado no teste
+    const numeros = [8, 10, 4, 3, 7, 9, 8, 2, 11,34 ];
+    // exibe a saída no console 
+    console.log('Exibindo os elementos de 2 em 2');
+    _.chunk(numeros,2).forEach((numero) =>{
+        console.log('--------');
+        numero.forEach((x)=>{
+            console.log(x);
+        })
+    });
+
+
