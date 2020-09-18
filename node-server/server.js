1ed0a8f2231a0c96e0b36ab140cff0d6acc42943
TadeunASCIImento <tadeunascimento36@gmail.com>
2020-09-17 23:26:59 -0300

    Exibindo as informações da requisição.

--- node-server/server.js
+++ node-server/server.js
@@ -7,9 +7,20 @@
         response.writeHead(200, { "Content-Type": "text/html" });
         response.write("<h1>Servidor acessado!</h1>");
         response.end();            
+        
+    // informações referntes a requisição    
+        const headers = request.headers;
+        const method = request.method;
+        const url = request.url;
+    // exibe no console
+        console.log("Headers");
+        console.log(headers);
+        console.log("Method: " + method);
+        console.log("URL: " + url);
+
     });
-    // porta do servido http://localhost:3000
-    servidor.listen(3000,()=>{
+    // porta do servidor http://localhost:3000
+        servidor.listen(3000,()=>{
         console.log('Servidor rodando na porta 3000');
     });
 
