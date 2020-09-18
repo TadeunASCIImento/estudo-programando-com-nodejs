37f11e1e5877c86f79c79650ed59aebb6a993a35
TadeunASCIImento <tadeunascimento36@gmail.com>
2020-09-18 11:15:32 -0300

    Importado o nodemon para o projeto.

--- npm-modules/index.js
+++ npm-modules/index.js
@@ -3,7 +3,7 @@
     const _ = require('lodash');
     
     // Array de números inteiros usado no teste
-    const numeros = [8, 10, 4, 3, 7, 9, 8, 2, 11,34 ];
+    const numeros = [8, 10, 4, 3, 7, 9, 8, 2, 11,34,23,78 ];
     // exibe a saída no console 
     console.log('Exibindo os elementos de 2 em 2');
     _.chunk(numeros,2).forEach((numero) =>{
